e45bb3085900f6ddb850c24da0498b7e8124e147
zewexqa <154309211+zewexqa@users.noreply.github.com>
2024-02-06 15:47:37 +0700

    Update lab17_1.cpp

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -3,28 +3,28 @@ using namespace std;
 
 int main(){
 	int nA,nB;
-	int *A,*B,*C;	
+	int *A,*B,**C;	
 	
 	cout << "Length of A: ";
 	cin >> nA;
 	
-	A = new int;
+	A = new int[nA];
 	cout << "Input Array A: ";
 	for(int i=0; i < nA; i++) cin >> A[i];
 	
 	cout << "Length of B: ";
 	cin >> nB;
 	
-	B = new int;
+	B = new int[nB];
 	cout << "Input Array B: ";
 	for(int i=0; i < nB; i++) cin >> B[i];
 	
-	C = new int;
-	for(int i=0; i < nA; i++) C[i] = new int;
+	C = new int*[nA];
+	for(int i=0; i < nA; i++) C[i] = new int[nB];
 	
 	for(int i=0; i < nA; i++){
 		for(int j=0; j < nB; j++) {
-			C = A[i]*B[j];
+			C[i][j] = A[i]*B[j];
 		}
 	}
 	
@@ -41,7 +41,10 @@ int main(){
 		}
 		cout << "\n";
 	}
-     delete A,B,C; 
+     delete [] A;
+     delete [] B;
+     for(int i=0; i< nA; i++) delete [] C[i];
+     delete [] C;
 
 	return 0;
 }
