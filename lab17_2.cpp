1167facb531126eac069a61ed3cd0df71cda6a63
zewexqa <154309211+zewexqa@users.noreply.github.com>
2024-02-06 15:48:15 +0700

    Update lab17_2.cpp

--- lab17_2.cpp
+++ lab17_2.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
+void myString(char *&p ,int N){
+	p = new char[N+1];    
+	for(int i = 0; i < N;i++) p[i] = 'A'+i;
+	p[N]=0;
 }
 
 int main(){
